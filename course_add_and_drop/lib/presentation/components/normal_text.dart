9bbc436678edd6f3818b3c0669908a24a345d3de
selam1630 <fikruselam29@gmail.com>
2025-06-11 10:36:01 +0300

    normal text

---
+++ course_add_and_drop/lib/presentation/components/normal_text.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class NormalTextComponent extends StatelessWidget {
+  final String value;
+
+  const NormalTextComponent({super.key, required this.value});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      value,
+      style: const TextStyle(
+        fontSize: 16,
+        color: Colors.black87,
+      ),
+    );
+  }
+}
