51a22e70ffebab61c5a3ad92e7d50dcd330a3200
Betsinat Amare <amarebetsinat@gmail.com>
2025-06-11 10:59:53 +0300

    added constants

---
+++ course_add_and_drop/lib/core/constants/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const colorPrimary = Colors.blue;
+  static const colorGrayBackground = Color(0xFFE0E0E0);
+  static const colorTextField = Colors.white;
+}
